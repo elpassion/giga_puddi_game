6099fc1778054b1fa779cbc6fb9c21d8e3c6056e
Patryk Wojtyński <patryk.wojtynski@elpassion.pl>
2015-09-30 17:14:08 +0200

    use port from env

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get('/', function (req, res) {
   res.render('layout');
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
